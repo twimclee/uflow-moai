dab96b1dfd4e4e8e1abc20e15e12868ab6956ef0
cylee <twimclee@gmail.com>
2025-01-14 17:13:33 +0900

    uflow for moai

--- train.py
+++ train.py
@@ -301,7 +301,7 @@ def train(args):
             patience=mhyp.patience,
         ),
     ]
-    logger = TensorBoardLogger(save_dir=mpfm.train_result, name=None)
+    logger = TensorBoardLogger(save_dir=mpfm.train_result, name=None, version=0)
     # shutil.copy(config_path, str(training_dir / "config.yaml"))
 
     trainer = Trainer(
