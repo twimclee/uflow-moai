1d2ef712b484c38da9889e93e7fa217a532b3632
cylee <twimclee@gmail.com>
2025-01-15 13:43:53 +0900

    uflow for moai

--- src/datamodule.py
+++ src/datamodule.py
@@ -89,7 +89,7 @@ class UFlowDataset(torch.utils.data.Dataset):
                 [
                     transforms.Resize(input_size),
                     transforms.ToTensor(),
-                    transforms.Normalize(mean.tolist(), std.tolist()),
+                    transforms.Normalize(self.mean.tolist(), self.std.tolist()),
                 ]
             )
 
